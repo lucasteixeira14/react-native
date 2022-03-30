b057232f1a9a1a02e528bef0f929946a77be5067
Lucas Teixeira <lucasteixeiramourasoares@hotmail.com>
2022-03-30 20:20:20 -0300

    feat: init react native

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var a= test();
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
