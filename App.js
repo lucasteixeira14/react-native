72a236fcf083659be9117e6fdee123ee87cc7461
Lucas Teixeira <lucasteixeiramourasoares@hotmail.com>
2022-04-10 22:33:31 -0300

    last

--- App.js
+++ App.js
@@ -71,7 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
-             <Text style={styles.highlight}>home</Text> 
+             <Text style={styles.highlight}>home test</Text> 
           </Section>
           <Section title="See ">
             <ReloadInstructions />
