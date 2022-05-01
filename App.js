e1a0c81a2de1e800f437427fc31f1f3541afa2fe
Lucas Teixeira <lucasteixeiramourasoares@hotmail.com>
2022-04-30 21:27:56 -0300

    last

--- App.js
+++ App.js
@@ -75,15 +75,7 @@ const App: () => Node = () => {
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
+
   highlight: {
     fontWeight: '700',
   },
