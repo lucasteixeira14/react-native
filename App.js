891546e3f234b56aedd4d2255bb98889536dd197
Lucas Teixeira <lucasteixeiramourasoares@hotmail.com>
2022-05-20 22:07:33 -0300

    last

--- App.js
+++ App.js
@@ -18,13 +18,6 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 
 
