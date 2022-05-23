f7091aeb142da6629a8780b0329da60edfe97339
Lucas Teixeira <lucasteixeiramourasoares@hotmail.com>
2022-05-22 22:47:24 -0300

    last

--- App.js
+++ App.js
@@ -8,15 +8,7 @@
 
 import React from 'react';
 import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+
 
 
 
